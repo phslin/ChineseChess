b1d920b6c3c0a334619cda5442a2766340ca5b85
Benson Lin <78063768+bensonms@users.noreply.github.com>
2025-08-07 21:13:25 -0700

    a

---
+++ ChineseChaseTests/ChineseChaseTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChineseChaseTests.swift
+//  ChineseChaseTests
+//
+//  Created by Benson Lin on 8/7/25.
+//
+
+import Testing
+@testable import ChineseChase
+
+struct ChineseChaseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
